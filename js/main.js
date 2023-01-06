cc5d6fd949d4c477b7a80305b7036152046962ad
Jill shen <pinkastro@gmail.com>
2023-01-06 16:21:21 +0800

    update homepage

--- js/main.js
+++ js/main.js
@@ -2,6 +2,32 @@
 const today = new Date();
 const hours = today.getHours();
 
+const cPosition = {
+  臺北市: '288 30',
+  新北市: '280 65',
+  桃園市: '240 55',
+  新竹縣: '235 95',
+  新竹市: '210 80',
+  苗栗縣: '210 130',
+  臺中市: '180 170',
+  彰化縣: '150 210',
+  雲林縣: '130 235',
+  嘉義縣: '170 265',
+  嘉義市: '142 265',
+  臺南市: '130 320',
+  高雄市: '130 380',
+  屏東縣: '170 420',
+  臺東縣: '225 350',
+  花蓮縣: '275 220',
+  南投縣: '220 200',
+  宜蘭縣: '300 115',
+  基隆市: '310 30',
+  澎湖縣: '15 250',
+  金門縣: '20 150',
+  連江縣: '20 80',
+};
+
+/* ------------------get weather api------------------------- */
 let weather = {
   apikey: 'CWB-76F302B7-FD21-4D85-BB7B-73FA0BEDBC2B',
 
@@ -142,43 +168,184 @@ let weather = {
         .parameterName;
     document.querySelector('.tomo-max-temp').innerText = tomomaxtemp + '°c';
   },
-
+  // search bar
   search: function () {
     this.fetchWeather(document.querySelector('.header-search-input').value);
-    document.querySelector('.city').innerText = document.querySelector(
-      '.header-search-input'
-    ).value;
+    const cityValue = document.querySelector('.header-search-input').value;
+    document.querySelector('.city').innerText = cityValue;
+
+    document
+      .getElementById('position')
+      .setAttribute('transform', `translate(${cPosition[cityValue]})`);
   },
 };
 
+// click search bar
 document
   .querySelector('.header-search-btn')
   .addEventListener('click', function () {
     weather.search();
   });
 
-const areas = document.querySelectorAll('.area');
+// 每日時間顯示
+const dateString = today.toLocaleDateString();
+const regex = /^(\d+).(\d+).(\d+)/;
+const [, year, month, day] = dateString.match(regex);
 
-areas.forEach((area) => {
-  area.addEventListener('click', () => {
-    const title = area.title;
-    // 將 title 傳遞給其他函數
-    weather.fetchWeather(title);
-    document.querySelector('.city').innerText = title;
+document.querySelector('.dateis').innerText = `${month}/${day}`;
+
+// 座標城市定位
+const gPosition = {
+  C63: '288 30',
+  C65: '280 65',
+  C68: '240 55',
+  C10004: '235 95',
+  C10018: '210 80',
+  C10005: '210 130',
+  C66: '180 170',
+  C10007: '150 210',
+  C10009: '130 235',
+  C10010: '170 265',
+  C10020: '142 265',
+  C67: '130 320',
+  C64: '130 380',
+  C10013: '170 420',
+  C10014: '225 350',
+  C10015: '275 220',
+  C10008: '220 200',
+  C10002: '300 115',
+  C10017: '310 30',
+  C10016: '15 250',
+  C09020: '20 150',
+  C09007: '20 80',
+};
+
+// 移動座標定位
+const gArea = document.querySelectorAll('g');
+
+gArea.forEach((g) => {
+  g.addEventListener('click', function () {
+    const value = gPosition[this.getAttribute('id')];
+    document
+      .getElementById('position')
+      .setAttribute('transform', `translate(${value})`);
   });
 });
 
-weather.fetchWeather('臺北市');
-
-const dateString = today.toLocaleDateString();
-document.querySelector('.dateis').innerText = dateString;
+// click map area
 
 const paths = document.querySelectorAll('path');
 
 paths.forEach((path) => {
-  path.addEventListener('mouseover', function () {
+  path.addEventListener('click', function () {
     weather.fetchWeather(this.getAttribute('data-name-zh'));
     document.querySelector('.city').innerText =
       this.getAttribute('data-name-zh');
   });
 });
+
+// 城市左右箭頭
+
+let indexValue = 0;
+function sideSlide(e) {
+  showCity((indexValue += e));
+}
+
+function showCity(e) {
+  const cityList = [
+    '臺北市',
+    '新北市',
+    '桃園市',
+    '新竹市',
+    '新竹縣',
+    '苗栗縣',
+    '臺中市',
+    '彰化縣',
+    '南投縣',
+    '雲林縣',
+    '嘉義市',
+    '嘉義縣',
+    '臺南市',
+    '高雄市',
+    '屏東縣',
+    '宜蘭縣',
+    '花蓮縣',
+    '臺東縣',
+    '澎湖縣',
+    '金門縣',
+    '連江縣',
+    '基隆市',
+  ];
+
+  if (e > cityList.length - 1) {
+    indexValue = 0;
+  }
+  if (e < 0) {
+    indexValue = cityList.length - 1;
+  }
+  weather.fetchWeather(cityList[indexValue]);
+  document.querySelector('.city').innerText = cityList[indexValue];
+
+  document
+    .getElementById('position')
+    .setAttribute('transform', `translate(${cPosition[cityList[indexValue]]})`);
+}
+
+// 圈圈天氣顯示
+const circles = document.querySelectorAll('.icon');
+
+circles.forEach((circle) => {
+  circle.addEventListener('click', function () {
+    weather.fetchWeather(this.getAttribute('data'));
+    document.querySelector('.city').innerText = this.getAttribute('data');
+
+    const value1 = gPosition[this.getAttribute('id')];
+    document
+      .getElementById('position')
+      .setAttribute('transform', `translate(${value1})`);
+  });
+});
+
+//首頁fetch
+weather.fetchWeather('臺北市');
+
+/* --------------首頁全台灣圈圈氣候fetch api------------------- */
+let picture = {
+  apikey: 'CWB-76F302B7-FD21-4D85-BB7B-73FA0BEDBC2B',
+
+  fetchWeather: function () {
+    fetch(
+      `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=${this.apikey}&format=JSON`
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    let parameterArray = data.records.location;
+    let valueArray = parameterArray.map(
+      (item) => item.weatherElement[0].time[0].parameter.parameterValue
+    );
+    let newValueArray = valueArray.map((item) =>
+      item < 10 ? (item = '0' + item) : item
+    );
+
+    const order = [
+      6, 2, 14, 4, 5, 9, 12, 21, 10, 1, 3, 7, 16, 18, 13, 11, 15, 8, 19, 20, 17,
+      22,
+    ];
+    const newArray = order.map((index) => newValueArray[index - 1]);
+
+    circles.forEach((circle, index) => {
+      if (hours < 6) {
+        circle.src = `https://www.cwb.gov.tw/V8/assets/img/weather_icons/weathers/svg_icon/night/${newArray[index]}.svg`;
+      } else if (hours < 18) {
+        circle.src = `https://www.cwb.gov.tw/V8/assets/img/weather_icons/weathers/svg_icon/day/${newArray[index]}.svg`;
+      } else if (hours < 24) {
+        circle.src = `https://www.cwb.gov.tw/V8/assets/img/weather_icons/weathers/svg_icon/night/${newArray[index]}.svg`;
+      }
+    });
+  },
+};
+
+//抓全台圈圈圖示
+picture.fetchWeather();
